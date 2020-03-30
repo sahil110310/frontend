8eb9e131efe89573c5c052946114d486c6c5084e
sahil110310 <sahilsavaliya110@gmail.com>
2020-03-30 23:25:44 +0530

    Updates

---
+++ precache-manifest.6e514d8fb82b0b4528086de2ae6803df.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "323bf72f2ce4902271c0749605a2a6ed",
+    "url": "/frontend/index.html"
+  },
+  {
+    "revision": "725650baac8a21ca1563",
+    "url": "/frontend/static/css/main.3393285a.chunk.css"
+  },
+  {
+    "revision": "2cbe243744a520484e85",
+    "url": "/frontend/static/js/2.4d62fb01.chunk.js"
+  },
+  {
+    "revision": "3e6c9780bd0354a71515f03a2f4c10a3",
+    "url": "/frontend/static/js/2.4d62fb01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "725650baac8a21ca1563",
+    "url": "/frontend/static/js/main.e1b17a0f.chunk.js"
+  },
+  {
+    "revision": "bb9cf7b820440dcc0565",
+    "url": "/frontend/static/js/runtime-main.e2bed40b.js"
+  },
+  {
+    "revision": "ff63ad61d10509fabeb13ec7e810e233",
+    "url": "/frontend/static/media/bg-01.ff63ad61.jpg"
+  },
+  {
+    "revision": "6f201314adb546f3beeffd64cc3df9d0",
+    "url": "/frontend/static/media/codechef.6f201314.jpeg"
+  }
+]);
